34baf81fe26ae1d60b35dff0c83020b925ffa89a
eymeric <eymericdechelette@gmail.com>
2024-05-31 23:52:15 +0200

    nextcloud ver

--- apps/nextcloud.nix
+++ apps/nextcloud.nix
@@ -1,10 +1,18 @@
-{pkgs, ...}: {
+{pkgs, config, ...}: {
+  age.secrets = {
+    nextcloudAdmin = {
+      file = ../secrets/nextcloudAdmin.age;
+      owner = "nextcloud";
+    };
+  };
+
   services.nextcloud = {
     enable = true;
+    package = pkgs.nextcloud29;
     hostName = "eymeric.eu";
     database.createLocally = true;
     config = {
-      adminpassFile = "/etc/nixos/adminpass";
+      adminpassFile = "${config.age.secrets.nextcloudAdmin}";
     };
   };
   networking.firewall.allowedTCPPorts = [80 443];
