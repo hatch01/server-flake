f13a6c7e64410b898a6384b3a02f4efd3287b3a0
eymeric <eymericdechelette@gmail.com>
2024-06-01 00:12:36 +0200

    cleanup and stronger password

--- apps/nextcloud.nix
+++ apps/nextcloud.nix
@@ -1,22 +1,44 @@
-{pkgs, config, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   age = {
     identityPaths = ["/etc/age/key"];
     secrets = {
-    nextcloudAdmin = {
-      file = ../secrets/nextcloudAdmin.age;
-      owner = "nextcloud";
+      nextcloudAdmin = {
+        file = ../secrets/nextcloudAdmin.age;
+        owner = "nextcloud";
+      };
     };
-  };
   };
 
-  services.nextcloud = {
-    enable = true;
-    package = pkgs.nextcloud29;
-    hostName = "eymeric.eu";
-    database.createLocally = true;
-    config = {
-      adminpassFile = config.age.secrets.nextcloudAdmin.path;
+  services = {
+    nextcloud = {
+      enable = true;
+      package = pkgs.nextcloud29;
+      hostName = "eymeric.eu";
+      autoUpdateApps.enable = true;
+      # https = true; # TODO when we have dns
+      configureRedis = true;
+      config = {
+        adminpassFile = config.age.secrets.nextcloudAdmin.path;
+      };
     };
   };
+
+  # when we have dns
+  #   services.nginx.virtualHosts.${config.services.nextcloud.hostName} = {
+  #   forceSSL = true;
+  #   enableACME = true;
+  # };
+
+  # security.acme = {
+  #   acceptTerms = true;
+  #   certs = {
+  #     ${config.services.nextcloud.hostName}.email = "your-letsencrypt-email@example.com";
+  #   };
+  # };
+
   networking.firewall.allowedTCPPorts = [80 443];
 }
