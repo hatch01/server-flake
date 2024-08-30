282132ddc4f7d556fa1804c3bf605e645b9991ec
eymeric <eymericdechelette@gmail.com>
2024-08-30 09:24:27 +0200

    chore: Remove podman-tui and docker-compose from configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -190,7 +190,6 @@ in {
     git
     inputs.agenix.packages.${system}.default
     dive # look into docker image layers
-    podman-tui # status of containers in the terminal
     #docker-compose # start group of containers for dev
     podman-compose # start group of containers for dev
     btop
