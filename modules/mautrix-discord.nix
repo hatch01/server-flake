875d297f0b00ba6a48073d55b700dd1d0e7e21d9
eymeric <eymericdechelette@gmail.com>
2024-09-01 22:21:15 +0200

    Revert "chore: Update mautrix-discord.nix with appservice address configuration"
    This reverts commit a681020e7ff73f5b147687b1dc71cfb621724169.

--- modules/mautrix-discord.nix
+++ modules/mautrix-discord.nix
@@ -18,7 +18,6 @@
   defaultConfig = {
     homeserver.address = "http://localhost:8448";
     appservice = {
-      address = "localhost:${toString appservicePort}";
       hostname = "[::]";
       port = appservicePort;
       database.type = "sqlite3";
