7f079371a751ce24630c09058a389991a0b69836
eymeric <eymericdechelette@gmail.com>
2024-09-10 21:49:06 +0200

    reformat

--- apps/matrix/signal.nix
+++ apps/matrix/signal.nix
@@ -20,7 +20,7 @@ in {
       registerToSynapse = true;
       settings = {
         bridge = {
-          displayname_template= "{{or .ProfileName .PhoneNumber \"Unknown user\"}} (Signal)";
+          displayname_template = "{{or .ProfileName .PhoneNumber \"Unknown user\"}} (Signal)";
           permissions = {
             "*" = "relay";
             "${hostName}" = "user";
